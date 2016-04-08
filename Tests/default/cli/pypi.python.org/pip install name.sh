fa3a2e7d8b50b333eeb6c04c84f887277ee73081
russianidiot <russianidiot.github@gmail.com>
2016-04-08 20:22:25 +0300

    - README.rst

--- Tests/default/cli/pypi.python.org/pip install name.sh
+++ Tests/default/cli/pypi.python.org/pip install name.sh
@@ -22,11 +22,18 @@ json=$(curl -s "$url") || exit 0 # no connection?
 }
 # ( set -x; pip install -U "$name" )
 # setuptools upgrade produce errors if installed from binary
-# pip <8: --no-use-wheel
-# pip 8+: --no-binary
-grep="$(pip install --help | grep "no-binary")"
-[[ -z $grep ]] && set -- pip install --no-use-wheel -U "$name"
-[[ -n $grep ]] && set -- pip install --no-binary -U "$name"
-
+# pip <7: --no-use-wheel
+# pip 7+: --no-binary
+pip="$(pip --version)" || exit
+IFS=' ';set $pip;IFS=
+version=$2
+IFS=.;set $version;IFS=
+major=$1;minor=$2;patch=$3
+echo major=$major
+echo minor=$minor
+echo patch=$patch
+set -- pip install --no-use-wheel -U "$name"
+[[ $major -ge 7 ]] set -- pip install --no-binary -U "$name"
+echo args=$@
 ( set -x; "$@" )
 
